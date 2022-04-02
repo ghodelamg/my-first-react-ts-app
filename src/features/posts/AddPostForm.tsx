91e7df39397db1d5821af0dbb50d3311ef251c1b
Mukesh <mukesh.easternts@gmail.com>
2022-04-02 20:31:03 +0530

    - Styled components

--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -2,6 +2,20 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { postAdded } from './postsSlice'
 import { useAppSelector } from '../../app/hooks'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import Container from '@mui/material/Container'
+import CssBaseline from '@mui/material/CssBaseline'
+import Box from '@mui/material/Box'
+import Avatar from '@mui/material/Avatar'
+import Typography from '@mui/material/Typography'
+import PostAddIcon from '@mui/icons-material/PostAdd';
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button';
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import InputLabel from '@mui/material/InputLabel'
+import FormControl from '@mui/material/FormControl'
+import Grid from '@mui/material/Grid'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('');
@@ -25,41 +39,85 @@ export const AddPostForm = () => {
   }
 
    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+   const theme = createTheme();
 
   const usersOptions = users.map(user => (
-    <option key={user.id} value={user.id}>
+    <MenuItem  key={user.id} value={user.id}>
       {user.name}
-    </option>
+    </MenuItem>
   ))
 
   return (
     <section>
-      <h2>Add a New Post</h2>
-      <form>
-        <label htmlFor="postTitle">Post Title:</label>
-        <input
-          type="text"
-          id="postTitle"
-          name="postTitle"
-          value={title}
-          onChange={onTitleChanged}
-        />
-        <label htmlFor="postAuthor">Author:</label>
-        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
-          <option value=""></option>
-          {usersOptions}
-        </select>
-        <label htmlFor="postContent">Content:</label>
-        <textarea
-          id="postContent"
-          name="postContent"
-          value={content}
-          onChange={onContentChanged}
-        />
-        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
-          Save Post
-        </button>
-      </form>
+      <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <PostAddIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Add a New Post
+          </Typography>
+          <Box component="form" onSubmit={onSavePostClicked} noValidate sx={{ mt: 1 }}>
+            <FormControl fullWidth sx={{ mb: 1 }}>
+                <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="postTitle"
+                label="Post Title"
+                name="postTitle"
+                autoFocus
+                value={title}
+                onChange={onTitleChanged}
+              />
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Post Author</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Post Author"
+                value={userId} onChange={onAuthorChanged}
+              >
+               {usersOptions}
+              </Select>
+            </FormControl>
+            <FormControl fullWidth>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="content"
+                label="Content"
+                id="content"
+                multiline
+                rows={4}
+                value={content}
+                onChange={onContentChanged}
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 2, mb: 1 }}
+              disabled={!canSave}
+            >
+              Save Post
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
     </section>
   )
 }
--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -1,37 +1,82 @@
 import { useAppSelector } from '../../app/hooks'
 import { AddPostForm } from './AddPostForm'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import Grid from '@mui/material/Grid'
+import Container from '@mui/material/Container'
+import CardActionArea from '@mui/material/CardActionArea'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
 
 const PostsList = () => {
   const posts = useAppSelector(state => state.posts)
 
   const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-
-const renderedPosts = orderedPosts.map(post => {
+  const navigate = useNavigate();
+ const theme = createTheme();
+  const RenderedPosts = () => {
   return (
-    <article className="post-excerpt" key={post.id}>
-      <h3>{post.title}</h3>
-      <div>
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-      </div>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-      <ReactionButtons post={post} />
-      <Link to={`/posts/${post.id}`} className="button muted-button">
-        View Post
-      </Link>
-    </article>
-  )
-})
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <main>
+          <Grid container spacing={4}>
+            { orderedPosts.map(post  => (
+               <Grid item xs={12} md={6} key={post.id} >
+                <CardActionArea component="a">
+                  <Card sx={{ display: 'flex' }}>
+                    <CardContent sx={{ flex: 1 }}>
+                      <Typography component="h2" variant="h5">
+                        {post.title}
+                      </Typography>
+                      <Typography variant="subtitle1" paragraph>
+                        <PostAuthor userId={post.userId} />
+                      </Typography>
+                      <Typography variant="subtitle1" color="text.secondary">
+                        <TimeAgo timestamp={post.date} />
+                      </Typography>
+                      <Typography variant="subtitle1" paragraph>
+                        <span className="post-content">{post.content.substring(0, 100)}</span>
+                      </Typography>
+                      <ReactionButtons post={post} />
+                      <Grid sx={{ mt: 3 }} container justifyContent="flex-end" onClick={ () => navigate(`/posts/${post.id}`)}>
+                        <Grid item style={{ textDecoration: 'underline' }}>
+                          View Post
+                          {/* <Link to={`/posts/${post.id}`} className="button muted-button">
+                            View Post
+                          </Link> */}
+                        </Grid>
+                      </Grid>
+                    </CardContent>
+                  </Card>
+                </CardActionArea>
+              </Grid>
+            ))}
+          </Grid>
+        </main>
+      </Container>
+    </ThemeProvider>
+    )};
 
   return (
     <section className="posts-list">
-      <AddPostForm></AddPostForm>
-      <h2>Posts</h2>
-      {renderedPosts}
+      <Container maxWidth="lg">
+        <main>
+          <Grid container spacing={2}>
+            <Grid item xs={12} lg={4}>
+              <AddPostForm></AddPostForm>
+            </Grid>
+            <Grid item xs={12} lg={8} sx={{ mt: 21 }}>
+              <RenderedPosts></RenderedPosts>
+            </Grid>
+          </Grid>
+        </main>
+      </Container>
     </section>
   )
 }
